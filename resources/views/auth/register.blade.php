24b5739236d0d3cd631810dd11589c5810faacb5
Zapupa <vitaliy2005rus93@gmail.com>
2025-05-22 12:24:32 +0500

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,84 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Имя')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="middlename" :value="__('Отчество')" />
+            <x-text-input id="middlename" class="block mt-1 w-full" type="text" name="middlename"
+                :value="old('middlename')" required autofocus autocomplete="middlename" />
+            <x-input-error :messages="$errors->get('middlename')" class="mt-2" />
+        </div>
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="lastname" :value="__('Фамилия')" />
+            <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')"
+                required autofocus autocomplete="lastname" />
+            <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
+        </div>
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="school" :value="__('Школа')" />
+            <x-text-input id="school" class="block mt-1 w-full" type="text" name="school" :value="old('school')"
+                required autofocus autocomplete="school" />
+            <x-input-error :messages="$errors->get('school')" class="mt-2" />
+        </div>
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="class" :value="__('Класс')" />
+            <x-text-input id="class" class="block mt-1 w-full" type="text" name="class" :value="old('class')" required
+                autofocus autocomplete="class" />
+            <x-input-error :messages="$errors->get('class')" class="mt-2" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Пароль')" />
+
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Подтвердить')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
+                {{ __('Уже зарегистрированы') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Зарегистрироваться') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
